14a41e80c3a41a5238fea1861c0a2902a3f8e2fc
eBottura29 <botturaenrico@gmail.com>
2024-12-17 15:50:59 +0100

    more clean up

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,5 @@
 # Had to learn matplotlib just for this project
-# Ur welcome (wasnt even that difficult)
+# You're welcome... (wasn't even that difficult)
 
 import matplotlib.pyplot as plt
 import csv
@@ -16,24 +16,19 @@ with open("training_data/points.csv", "r") as f:
         batches.append(int(row[0]))
         costs.append(float(row[1]))
 
-# Fit a higher-order polynomial (e.g., cubic) to the cost data
-degree = 10  # Degree of the polynomial for the trend line
+# Fit a higher-order polynomial to the cost data
+degree = 10  # Degree of the polynomial
 trend_coeffs = np.polyfit(batches, costs, degree)
 trend_line = np.poly1d(trend_coeffs)
 
 # Generate trend line values
 trend_costs = trend_line(batches)
 
-# Plot the cost evolution
 plt.plot(batches, costs, label="Cost", color="blue")
 plt.plot(batches, trend_costs, label="Trend Line", color="red", linestyle="--")
-
-# Add labels, title, and legend
 plt.xlabel("Batch")
 plt.ylabel("Average Cost")
 plt.title("Cost Evolution During Training with Trend Line")
 plt.legend()
 plt.grid()
-
-# Show the plot
 plt.show()
